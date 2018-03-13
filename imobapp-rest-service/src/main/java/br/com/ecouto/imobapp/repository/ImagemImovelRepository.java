9cce4ebdd21156b863b72253d96b4acdd5626c21
enzocouto <enzocouto@gmail.com>
2018-03-13 15:56:10 -0300

    removenmdo diretortio duplicado

--- imobapp-rest-service/src/main/java/br/com/ecouto/imobapp/repository/ImagemImovelRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.ecouto.imobapp.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.ecouto.imobapp.entity.ImagemImovel;
-
-public interface ImagemImovelRepository extends MongoRepository<ImagemImovel, String> {
-
-}
